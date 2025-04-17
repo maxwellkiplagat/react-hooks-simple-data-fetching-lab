81a919b2ad4ab2aa6f91582252baaa58ff8a5763
maxwellkiplagat <maxwellkiplagat0@gmail.com>
2025-04-17 12:19:48 +0300

    completed lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,26 @@
 // create your App component here
+import React, { useEffect, useState } from "react";
+
+function App() {
+  const [dogImage, setDogImage] = useState(null);
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((res) => res.json())
+      .then((data) => {
+        setDogImage(data.message); // API returns image URL in `message`
+      });
+  }, []); // Empty array means this runs once on mount
+
+  return (
+    <div>
+      {dogImage ? (
+        <img src={dogImage} alt="A Random Dog" />
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+}
+
+export default App;
